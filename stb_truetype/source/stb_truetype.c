4de5291dc8e7d1c6f66a4fb5c205d27e4c6b696a
acoto87 <acoto87@gmail.com>
2021-01-10 17:27:05 -0600

    Change licence from MIT to ZLib and include copyright notice in the file headers.

--- stb_truetype/source/stb_truetype.c
+++ stb_truetype/source/stb_truetype.c
@@ -1,2 +1,11 @@
+//
+// A simple C file to be able to compile stb_truetype into a dynamic (.dll) and static (.lib).
+//
+// The modifications of the original stb_truetype library includes:
+//
+// Defining STBTT_EXPORT as
+//      #define STBTT_EXPORT __declspec(dllexport)
+// and mark several functions for dll exporting, see module.jai of this library for more information.
+//
 #define STB_TRUETYPE_IMPLEMENTATION  // force following include to generate implementation
 #include "stb_truetype.h"
