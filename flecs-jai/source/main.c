67e48b41ce16dabee69cdebc0c5c111980cb8c2b
acoto87 <acoto87@gmail.com>
2021-07-11 03:17:25 -0500

    - Add sparse_map.jai, quadtree.jai and kdtree.jai. - Rename map.jai to hash_map.jai. - Move color.jai to utils module. - Add circle.jai - Incomplete bindings for flecs. - Move dlls to respective lib/win64 folders.

---
+++ flecs-jai/source/main.c
@@ -0,0 +1,8 @@
+//
+// A simple C file to be able to compile flecs into a dynamic (.dll) and static (.lib).
+//
+// The flecs.h and flecs.c files are from commit 6fb148466afeac1b2f3269c72776ac0b38d462af
+//
+#define FLECS_NO_CPP 1
+#define flecs_EXPORTS 1
+#include "flecs.h";
